fd16a4ef04adbffb28c60d711b0e1417ffd04c74
raulvalemartins <raulvalemartins@gmail.com>
2015-02-17 19:34:08 +0000

    tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## the 'makeCacheMatrix' function creates a special object as 
+## that stores a matrix and cache's its inverse 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( x = matrix()) {
+        
+        i <- NULL
+        set <- function (y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function () x
+        getInverse <- function () i
+        setInverse <- function (inverse) i <<- inverse
+        list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## the 'cacheSolve' function calculates de inverse matrix create by the 'makeCacheMatrix'.
+## Before the calculation proceds this function checks to see if the inverse has already 
+## been calculated and at same time if the content of the matrix is the same. 
+## If so, it gets the inverse matrix from the cache and skips 
+## the computation. Otherwise, it calculates the inverse matrix of the data and 
+## sets the value of the inverse matrix in the cache via the 'setInverse' function
+## the variables: 'originalData' and 'originalObject' auxliates the determination if  
+## the content of the matrix was not changed.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+        
+        data <- x$get()
+        
+        if (identical(originalData, data )) {
+                m <- originalObject$getInverse()
+                print("getting cached data 1")
+                return(m)
+        } else
+        {
+                m <- x$getInverse()
+                if(!is.null(m) ) {
+                        ##       ##message("getting cached data")
+                        print("getting cached data2 ")
+                        originalData <<- data
+                        originalObject <<- x
+                        return(m)
+                }
+        }
+                 
+        m <- solve(data, ...)
+        x$setInverse(m)
+        originalData <<- data
+        originalObject <<- x
+        m
 }
