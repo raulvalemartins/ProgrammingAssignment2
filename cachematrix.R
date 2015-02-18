59615827e7d90bcebc3407222b55fa04370d8254
raulvalemartins <raulvalemartins@gmail.com>
2015-02-18 13:13:11 +0000

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+## HOW TO TEST ALL THE CENARIOS
 
-## the 'makeCacheMatrix' function creates a special object as 
-## that stores a matrix and cache's its inverse 
+##      SECTION 1: Run the next four commands until SECTION 2
+
+##      m1 <- matrix(data = 1:4, nrow = 2, ncol = 2)
+##      m2 <- matrix(data = 5:8, nrow = 2, ncol = 2)
+##      matrixCache <- makeCacheMatrix( m1 )
+##      cacheSolve( matrixCache )
+##      
+##      Note: at this moment if you run 'cacheSolve( matrixCache )' several times
+##      the inverse cached value is always used because the inverse matrix has 
+##      already been calculated and at same time the matrix has not changed
+
+##      SECTION 2: Now, run the next two commands.
+
+##      matrixCache$setMatrix( m2 )
+##      cacheSolve( matrixCache )
+##
+##      Note: the last call to the 'cacheSolve( matrixCache )' doesn't use 
+##      the chached value of inverse matrix, because the new matrix is not the 
+##      same that was previously cached.
+
+##      RESULTS:
+##      for these testcases the inversed matrices are:
+##      Inverse of m1
+##              [,1] [,2]
+##      [1,]   -2  1.5
+##      [2,]    1 -0.5
+
+##      Inverse of m2
+##              [,1] [,2]
+##      [1,]   -4  3.5
+##      [2,]    3 -2.5 
 
 makeCacheMatrix <- function( x = matrix()) {
         
+        ## the 'makeCacheMatrix' function creates a special object
+        ## that stores a matrix and cache's its inverse
+        
         i <- NULL
-        set <- function (y) {
+
+        setMatrix <- function (y = NULL)  {
+                lastMatrix <<- x
                 x <<- y
-                i <<- NULL
         }
-        get <- function () x
+        getMatrix <- function () x
+        getLastMatrix <- function () lastMatrix
         getInverse <- function () i
         setInverse <- function (inverse) i <<- inverse
-        list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
+        list(setMatrix = setMatrix, getMatrix = getMatrix, 
+             getLastMatrix = getLastMatrix, 
+             getInverse = getInverse, setInverse = setInverse )
 }
-
-
-## the 'cacheSolve' function calculates de inverse matrix create by the 'makeCacheMatrix'.
-## Before the calculation proceds this function checks to see if the inverse has already 
-## been calculated and at same time if the content of the matrix is the same. 
-## If so, it gets the inverse matrix from the cache and skips 
-## the computation. Otherwise, it calculates the inverse matrix of the data and 
-## sets the value of the inverse matrix in the cache via the 'setInverse' function
-## the variables: 'originalData' and 'originalObject' auxliates the determination if  
-## the content of the matrix was not changed.
 
 cacheSolve <- function(x, ...) { 
         
-        data <- x$get()
-        
-        if (identical(originalData, data )) {
-                m <- originalObject$getInverse()
+        ## the 'cacheSolve' function calculates de inverse matrix create by the 
+        ## 'makeCacheMatrix'. Before the calculation proceds this function 
+        ## checks to see if the inverse has already been calculated and at 
+        ## same time if the content of the matrix is the same. 
+          
+        m <- x$getInverse()
+        if(!is.null(m) && identical(x$getMatrix(), x$getLastMatrix())) {
+                
                 message("getting cached data")
                 return(m)
-        } else
-        {
-                m <- x$getInverse()
-                if(!is.null(m) ) {
-                        message("getting cached data")
-                        originalData <<- data
-                        originalObject <<- x
-                        return(m)
-                }
         }
-                 
+        
+        data <- x$getMatrix()
         m <- solve(data, ...)
         x$setInverse(m)
-        originalData <<- data
-        originalObject <<- x
+        x$setMatrix (data)
         m
 }
